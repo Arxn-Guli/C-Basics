9c82fbfa74a51bbe27d2f61d7ad8731d67fd7167
Arxn <arxn1688@163.com>
2024-09-23 06:03:28 +0800

    End

--- Lesson 1/Program.cs
+++ Lesson 1/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int Height = Console.WindowHeight;
+
+Console.WriteLine("This WindowHeighet : " + Height);
+
+Console.WindowHeight = 30;
+
+Console.Clear();
